b2d8fe82b20dc4940da1d9affa28fce79f416b14
techprocodepro <techprocodepro@gmail.com>
2025-01-19 00:04:12 +0530

    count issue

--- frontend/src/Components/Wallet.jsx
+++ frontend/src/Components/Wallet.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import protocol from '../Assets/protocol.png'
-import {CountUp} from 'countup.js'
+import { CountUp } from 'countup.js'
 
 const Wallet = () => {
 
     const walletAmount = useSelector(state => state.walletAmount);
     const [displayAmount, setDisplayAmount] = useState(walletAmount);
-    let count = displayAmount;
+    // let count;
+    // setDisplayAmount(prev => { count = prev })
 
     useEffect(() => {
         const countUp = new CountUp('count', walletAmount, {
-            duration,
-            startVal: count,
+            duration: 1,
+            startVal: displayAmount,
             useEasing: true,
         });
         countUp()
