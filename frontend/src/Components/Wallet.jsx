96f479f28b219b80d12319ddf7c027ddabf06f49
techprocodepro <techprocodepro@gmail.com>
2025-01-19 00:10:04 +0530

    count isse

--- frontend/src/Components/Wallet.jsx
+++ frontend/src/Components/Wallet.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import protocol from '../Assets/protocol.png'
-import {CountUp} from 'countup.js'
 
 const Wallet = () => {
 
     const walletAmount = useSelector(state => state.walletAmount);
     const [displayAmount, setDisplayAmount] = useState(walletAmount);
-    let count = displayAmount;
 
     useEffect(() => {
-        const countUp = new CountUp('count', walletAmount, {
-            duration:1,
-            startVal: count,
-            useEasing: true,
-        });
-        countUp()
+
+        if (displayAmount !== walletAmount) {
+            const interval = setInterval(() => {
+                setDisplayAmount(prevAmount => {
+                    if (prevAmount < walletAmount) {
+                        return prevAmount + 1;  // Increase the display amount when the wallet increases
+                    } else if (prevAmount > walletAmount) {
+                        return prevAmount - 1; // Decrease the display amount when the wallet decreases
+                    }
+                    clearInterval(interval);
+                    return prevAmount; // Stop when displayAmount reaches walletAmount
+                });
+            }, 1);
+
+            // Clear the interval when the component unmounts or walletAmount has finished animating
+            return () => clearInterval(interval);
+        }
     }, [walletAmount, displayAmount]);
 
     return (
